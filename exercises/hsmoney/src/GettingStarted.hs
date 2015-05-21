8a18f0472bf334dd460dcb5fd551c88d445754bc
Willem van den Ende <willem@qwan.eu>
2015-05-21 11:51:57 +0100

    Add exercise directory for participants

---
+++ exercises/hsmoney/src/GettingStarted.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE ScopedTypeVariables #-} -- We will explain why we need this later
+module GettingStarted where
+
+runTests = do
+  putStrLn "something along the lines of: quickCheck prop_SquareRootOfNSquaredEqualsN goes here"
+
---
+++ exercises/hsmoney/src/joy.hs
@@ -0,0 +1,8 @@
+module Main where
+import GettingStarted as S
+
+-- Manually aggregated property suite.
+
+main = do
+ S.runTests
+ -- add your own modules to  run here after "Getting Started". You may want to add new modules to ../hsmoney.cabal as well
