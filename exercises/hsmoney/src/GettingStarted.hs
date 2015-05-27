e15e3dad6dab85b04b8105fe251052a7968f5d40
Willem van den Ende <willem@qwan.eu>
2015-05-27 17:58:37 +0100

    Length correct, with fake division of money

--- exercises/hsmoney/src/GettingStarted.hs
+++ exercises/hsmoney/src/GettingStarted.hs
@@ -1,6 +1,14 @@
 {-# LANGUAGE ScopedTypeVariables #-} -- We will explain why we need this later
-module GettingStarted where
+module GettingStarted (runTests) where
+import Test.QuickCheck
 
+payDutch :: Int -> Int -> [Int]
+payDutch amount people = take people $ repeat amount
+
+prop_dutchLength :: NonNegative Int -> NonNegative Int -> Property
+prop_dutchLength (NonNegative n) (NonNegative len) = (length $ payDutch n len) === len
+
+runTests :: IO ()
 runTests = do
-  putStrLn "something along the lines of: quickCheck prop_SquareRootOfNSquaredEqualsN goes here"
+  quickCheck prop_dutchLength
 
