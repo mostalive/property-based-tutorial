57727bff4c776839d94b65ad567ba13d0cf0c5f4
Willem van den Ende <willem@qwan.eu>
2015-05-28 21:19:19 +0200

    Revert "Length correct, with fake division of money"
    This reverts commit 01fb9f18c78419bba983240db10b3e46cd3be5c6.

--- exercises/hsmoney/src/GettingStarted.hs
+++ exercises/hsmoney/src/GettingStarted.hs
@@ -1,14 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-} -- We will explain why we need this later
-module GettingStarted (runTests) where
-import Test.QuickCheck
+module GettingStarted where
 
-payDutch :: Int -> Int -> [Int]
-payDutch amount people = take people $ repeat amount
-
-prop_dutchLength :: NonNegative Int -> NonNegative Int -> Property
-prop_dutchLength (NonNegative n) (NonNegative len) = (length $ payDutch n len) === len
-
-runTests :: IO ()
 runTests = do
-  quickCheck prop_dutchLength
+  putStrLn "something along the lines of: quickCheck prop_SquareRootOfNSquaredEqualsN goes here"
 
